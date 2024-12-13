8813cba9a98ab9bfa24316b77b02ca9bd7931400
Aleksei Lazunin <gegcuk@gmail.com>
2024-12-13 23:46:57 +0000

    Add MathConstants class with final static Pi and E as properties.

---
+++ src/main/java/uk/gegc/effectiveJava/part5/MathConstants.java
@@ -0,0 +1,8 @@
+package uk.gegc.effectiveJava.part5;
+
+public class MathConstants {
+    private MathConstants() { } // Запрет на создание экземпляра
+	public static final double PI = 3.141_592_653_589_793;
+	public static final double E = 2.718_281_828_459_045;
+}
+
